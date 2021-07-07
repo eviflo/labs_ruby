193f13650e13ced0ad36c2bb80f9ee29ac3d238a
eviflo <daryanemenko@yandex.ru>
2021-07-07 10:59:15 +0200

    two labs done

---
+++ lab2.rb
@@ -0,0 +1,8 @@
+print "Введите число x: "
+x=gets.chomp.to_i
+print "Введите число y: "
+y=gets.chomp.to_i
+if  x==20 then print "x=#{x}" 
+elsif y == 20 then print "y=#{y}" 
+else print "Ни одно из введенных чисел не равно 20, x+y=#{x+y}"
+end
