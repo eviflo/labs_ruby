20028aceec5479500202791162ed4ea16d8a32c7
eviflo <daryanemenko@yandex.ru>
2021-07-13 18:02:47 +0200

    pokemons

--- lab4.rb
+++ lab4.rb
@@ -1,14 +1,13 @@
 print "Введите число покемонов:"
-@pokemon_num=gets.chomp.to_i
-@pokemon_num.times {
-
-print "Введите имя покемона:"
-@pokemon_name=gets.chomp
-print "Введите цвет покемона:"
-@pokemon_color=gets.chomp
-
-pokemons= {:name =>  @pokemon_name, :color => @pokemon_color}
-pokemons.each do |key, value|
-  puts pokemons
+pokemon_num = gets.chomp.to_i
+# создаем массив для покемонов
+pokemons = Array.new
+pokemon_num.times do 
+  print "Введите имя покемона:"
+  pokemon_name=gets.chomp
+  print "Введите цвет покемона:"
+  pokemon_color=gets.chomp
+  pokemons.push ({:name => pokemon_name, :color => pokemon_color})
 end
-}
+pokemons.each{|pokemons| puts pokemons }
+
