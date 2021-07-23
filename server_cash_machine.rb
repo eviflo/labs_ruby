f24a6eb0972606a08712a76bb056fbb5cb9dbd83
eviflo <daryanemenko@yandex.ru>
2021-07-23 03:27:58 +0200

    LABS READY!

--- server_cash_machine.rb
+++ server_cash_machine.rb
@@ -6,12 +6,21 @@ require 'rack/utils'
 
 server = TCPServer.new('0.0.0.0', 3000)
 def balance
-  @start_balance = 100
+  balance = 100
   if File.exist?('./data/balance.txt')
     file = File.open('./data/balance.txt', 'r')
-    @balance = File.read('./data/balance.txt').to_i
-  else puts @balance == @start_balance
+    balance = File.read('./data/balance.txt').to_i
   end
+  balance
+end
+
+def load_balance
+  balance = 100
+  if File.exist?('./data/balance.txt')
+    file = File.open('./data/balance.txt', 'r')
+    balance = File.read('./data/balance.txt').to_i
+  end
+  balance
 end
 
 class App
@@ -22,31 +31,43 @@ class App
     when '/'
             [200, { 'Content-Type' => 'text/html', 'encoding' => 'utf-8' }, ["Your start balance is #{balance}"]]
     when '/deposit'
-            [200, { 'Content-Type' => 'text/html' }, ["What sum do you want to add to your balance? #{deposit_plus}"]]
+            [200, { 'Content-Type' => 'text/html' }, ["What sum do you want to add to your balance? #{deposit_plus(req.params['value'].to_f)}"]]
     when '/withdraw'
-      [200, { 'Content-Type' => 'text/html' }, ['What sum do you want to withdraw from your balance?']]
+      [200, { 'Content-Type' => 'text/html' }, ["What sum do you want to withdraw from your balance? #{deposit_withdraw(req.params['value'].to_f)}"]]
     else
-      [404, { 'Content-Type' => 'text/html' }, ['Not found']]
+      [404, { 'Content-Type' => 'text/html' }, ["Not found"]]
     end
   end
 end
 
-def deposit_plus
-  req = Rack::Request.new
-  deposit_sum = req.params['value']
+def deposit_plus(deposit_sum)
+  balance = load_balance
   if deposit_sum >= 0
-    puts @balance + deposit_sum
-    print "Сумма текущего баланса #{@balance} + #{deposit_sum} =#{@balance + deposit_sum} \n"
+    puts balance + deposit_sum
+    print "Сумма текущего баланса #{balance} + #{deposit_sum} =#{balance + deposit_sum} \n"
     File.open('./data/balance.txt', 'w+') do |file|
-      file.write((@balance + deposit_sum).to_s)
+      file.write((balance + deposit_sum).to_s)
       print "Данные записаны\n"
       file.close
     end
   end
 end
 
-app = App.new
+def deposit_withdraw(withdraw_sum)
+  balance = load_balance
+    if withdraw_sum <= balance
+    balance - withdraw_sum
+    print "Сумма текущего баланса #{balance} - #{withdraw_sum} =#{balance - withdraw_sum}\n"
+    File.open('./data/balance.txt', 'w+') do |file|
+      file.write((balance -withdraw_sum).to_s)
+      print "Данные записаны \n"
+      file.close
+    end
+  else print 'Вы ввели некорректное значение, перезапустите программу'
+  end
+end
 
+app = App.new
 while connection = server.accept
   request = connection.gets
   method, full_path = request.split(' ')
