def4c73a3b3d51fde8d1a82dff25097cb395397a
eviflo <daryanemenko@yandex.ru>
2021-07-23 02:15:58 +0200

    server_again_2

--- server_cash_machine.rb
+++ server_cash_machine.rb
@@ -7,28 +7,40 @@ require 'rack/utils'
 server = TCPServer.new('0.0.0.0', 3000)
 def balance
   @start_balance = 100
-  if @balance = File.exist?('./data/balance.txt')
+  if File.exist?('./data/balance.txt')
     file = File.open('./data/balance.txt', 'r')
     @balance = File.read('./data/balance.txt').to_i
   else puts @balance == @start_balance
   end
 end
 
-def deposit_plus
- end
-
 class App
   def call(env)
     req = Rack::Request.new(env)
-    case req.path
+    req.params['value']
+        case req.path
     when '/'
             [200, { 'Content-Type' => 'text/html', 'encoding' => 'utf-8' }, ["Your start balance is #{balance}"]]
     when '/deposit'
-            [200, { 'Content-Type' => 'text/html' }, ["What sum do you want to add to your balance?"]]
+            [200, { 'Content-Type' => 'text/html' }, ["What sum do you want to add to your balance? #{deposit_plus}"]]
     when '/withdraw'
       [200, { 'Content-Type' => 'text/html' }, ['What sum do you want to withdraw from your balance?']]
     else
       [404, { 'Content-Type' => 'text/html' }, ['Not found']]
+    end
+  end
+end
+
+def deposit_plus
+  req = Rack::Request.new
+  deposit_sum = req.params['value']
+  if deposit_sum >= 0
+    puts @balance + deposit_sum
+    print "Сумма текущего баланса #{@balance} + #{deposit_sum} =#{@balance + deposit_sum} \n"
+    File.open('./data/balance.txt', 'w+') do |file|
+      file.write((@balance + deposit_sum).to_s)
+      print "Данные записаны\n"
+      file.close
     end
   end
 end
@@ -38,14 +50,14 @@ app = App.new
 while connection = server.accept
   request = connection.gets
   method, full_path = request.split(' ')
-  path = full_path.split('?')[0]
-
+  parts = full_path.split('?')
   status, headers, body = app.call({
-                                     'REQUEST_METHOD' => method,
-                                     'PATH_INFO' => path
-                                   })
-
-  connection.print("HTTP/1.1 #{status}\r\n")
+'REQUEST_METHOD' => method,
+'rack.input' => '',
+'PATH_INFO' => parts.first,
+'QUERY_STRING' => parts.last
+})
+    connection.print("HTTP/1.1 #{status}\r\n")
 
   headers.each { |key, value| connection.print("#{key}: #{value}\r\n") }
 
