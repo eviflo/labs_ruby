7f90601bc9e029f4e64a596986a6b4223a3b2946
eviflo <daryanemenko@yandex.ru>
2021-11-29 14:38:15 +0300

    fixed code style

--- lab1/lab1.rb
+++ lab1/lab1.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 def greeting
   puts 'Enter your name: '
   name = gets
@@ -5,9 +7,11 @@ def greeting
   surname = gets
   puts 'Enter your age:'
   age = gets.to_i
-  age >=	18 ? "Hello, #{name} #{surname}!!! It's time to start with programming!" :
+  if age >=	18
+    "Hello, #{name} #{surname}!!! It's time to start with programming!"
+  else
     "Hello, #{name} #{surname}!!! You are less than 18 years old, but it's never late to start with programming."
+  end
 end
 
 puts greeting
-
